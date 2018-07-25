d6cc44d9c773523bda83e3460b22b7e58c3b4b95
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-24 17:54:32 -0700

    Completed through Step 2

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,40 +1,45 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, flash
 import hackbright
 
 import hackbright
 
 app = Flask(__name__)
 
+app.secret_key = "This is a secret."
 
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = request.args.get('github')
+    # import pdb; pdb.set_trace();
+    if 'new_student' in session:
+        github = session['new_student'][2]
 
-    first, last, github = hackbright.get_student_by_github(github)
+        first_name, last_name, github = hackbright.get_student_by_github(github)
 
-    html = render_template("student_info.html",
-                           first=first,
-                           last=last,
-                           github=github)
-    return html
+        html = render_template("student_info.html",
+                       first=first_name,
+                       last=last_name,
+                       github=github)
+        return html
+    else:
+        return redirect("/student-add-form")
 
 
 @app.route("/student-search")
 def get_student_form():
-	"""Show form for searching for a student."""
+    """Show form for searching for a student."""
 
-	return render_template("student_search.html")
+    return render_template("student_search.html")
 
 
 @app.route("/student-add-form")
 def student_add_form():
-	"""Show form for adding a student."""
+    """Show form for adding a student."""
 
-	return render_template("add_student.html")
+    return render_template("add_student.html")
 
 
 @app.route("/student-add", methods=['POST'])
@@ -47,11 +52,16 @@ def student_add():
     hackbright.make_new_student(first_name,last_name,github)
 
     html = render_template("show_added_student.html",
-    						first_name=first_name,
-    						last_name=last_name,
-    						github=github)
+                            first_name=first_name,
+                            last_name=last_name,
+                            github=github)
+
+    session['new_student'] = (first_name, last_name, github)
+    # flash("")
 
     return html
+
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
